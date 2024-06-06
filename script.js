896f72275c9ee0326e0b1112e9a68724f4cb19ee
sgarbossabeatriz <110116950+sgarbossabeatriz@users.noreply.github.com>
2024-06-06 09:08:03 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -19,22 +19,22 @@ const perguntas = [
         ]
     },
     {
-        enunciado: "Com a descoberta desta tecnologia, chamada Inteligência Artificial, uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre esta tecnologia. No fim de uma aula ela pede que você escreva um trabalho sobre o uso de IA em sala de aula. Qual atitude você toma?",
+        enunciado: "Ao longo do relato histórico, seria importante destacar não apenas a evolução da pandemia em si, mas também a forma como a ciência e a medicina têm respondido a essas variantes em constante mutação.",
         alternativas: [
             {
-                texto: "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
+                texto: "Como as vacinas contra o COVID-19 estão sendo adaptadas para lidar com as variantes emergentes do vírus?",
                 afirmacao: "Conseguiu utilizar a IA para buscar informações úteis."
             },
             {
-                texto: "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema.",
+            
                 afirmacao: "Sentiu mais facilidade em utilizar seus próprios recursos para escrever seu trabalho."
             }
         ]
     },
     {
-        enunciado: "Após a elaboração do trabalho escrito, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
-        alternativas: [
-            {
+        enunciado: " As vacinas contra a COVID-19 estão sendo adaptadas para lidar com as variantes emergentes do vírus de várias maneiras: atualizações das vacinas existentes para melhorar sua eficácia contra as variantes, estudos para avaliar a eficácia das vacinas contra as variantes, desenvolvimento de vacinas multivalentes e consideração de doses de reforço. Essas medidas visam garantir uma proteção contínua contra a evolução do SARS-CoV-2. ",
+        alternativas: [    
+        {
                 texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
                 afirmacao: "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA."
             },
